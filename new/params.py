b2b54eb165a345c500af17b4ff01d738b22f1280
Yizhou <yizhou_zhao@berkeley.edu>
2021-10-27 12:42:46 -0700

    tuning parameter

--- new/params.py
+++ new/params.py
@@ -1,5 +1,5 @@
 ###################   network    ############################
-z_dim = 2048 # may change to 1024
+z_dim = 128 # may change to 1024
 point_num = 2048
 
 e_l_steps  = 80 # number of langevin steps
